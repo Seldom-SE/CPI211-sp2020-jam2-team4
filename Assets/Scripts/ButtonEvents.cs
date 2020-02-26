d34bfc431bcb0a703722cab848abe8d85ac69d99
Seldom <ssaguUUUU@gmail.com>
2020-02-25 21:24:49 -0700

    Title Screen

---
+++ Assets/Scripts/ButtonEvents.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class ButtonEvents : MonoBehaviour
+{
+    public void PlayButton () {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void QuitButton () {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void TitleScreenButton () {
+        SceneManager.LoadScene("TitleScreen");
+    }
+}
